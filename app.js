496510a789045b08742655cbb478944622348fd2
ankitsejwal <ankit@sejw.al>
2023-05-09 19:26:29 +0530

    added config

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const config = require('config');
 const todos = require('./routes/todos');
 
 const app = express();
 
 app.use('/api/todos', todos);
 
+const env = app.get('env');
+console.log(env);
+
+console.log(config.get('mongodb.password'));
+
 app.get('/', (req, res) => {
-  const env1 = app.get('env');
-  res.send(env1);
+  res.send('home route');
 });
 
 const port = process.env.PORT || 3000;
