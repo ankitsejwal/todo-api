d87e19838af0888919026573f4ec183556e75424
ankitsejwal <ankit@sejw.al>
2023-05-11 14:28:10 +0530

    get rid of unwanted code

--- app.js
+++ app.js
@@ -6,14 +6,5 @@ const app = express();
 
 app.use('/api/todos', todos);
 
-const env = app.get('env');
-console.log(env);
-
-console.log(config.get('mongodb.password'));
-
-app.get('/', (req, res) => {
-  res.send('home route');
-});
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('port:', port));
--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Todo = require('../models/todos');
+
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.status(200).send('works');
+  const todos = await Todo.find();
+  res.status(200).send(todos);
 });
 
 router.get('/:id', async (req, res) => {});
