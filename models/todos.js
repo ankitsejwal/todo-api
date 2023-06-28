629a9bff80ca61a9f231d1d497e67ef661804bed
ankitsejwal <ankit@sejw.al>
2023-06-28 16:20:12 +0530

    code cleanup

--- models/todos.js
+++ models/todos.js
@@ -1,15 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-mongoose
-  .connect(
-    `mongodb+srv://${config.get('MONGO_USERNAME')}:${config.get(
-      'MONGO_PASSWORD'
-    )}@vidly.zivn542.mongodb.net/vidly`
-  )
-  .then(() => console.log('connected to mongodb'))
-  .catch((err) => console.log(err));
-
 const todoSchema = new mongoose.Schema({
   name: {
     type: String,
--- models/users.js
+++ models/users.js
@@ -2,15 +2,6 @@ const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const config = require('config');
 
-mongoose
-  .connect(
-    `mongodb+srv://${config.get('MONGO_USERNAME')}:${config.get(
-      'MONGO_PASSWORD'
-    )}@vidly.zivn542.mongodb.net/vidly`
-  )
-  .then(() => console.log('connected to mongodb'))
-  .catch((err) => console.log(err));
-
 const userSchema = new mongoose.Schema({
   name: { type: String, min: 3, max: 18, require: true },
   email: { type: String, min: 8, max: 50, require: true },
@@ -20,10 +11,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.genAuthToken = function () {
-  return jwt.sign(
-    { _id: this._id, isAdmin: this.isAdmin },
-    config.get('JWT_PVT_KEY')
-  );
+  return jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('JWT_PVT_KEY'));
 };
 
 module.exports = mongoose.model('User', userSchema);
