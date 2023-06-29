cc52a8228bd4e60da20987f10e4d5749b9c092f0
ankitsejwal <ankit@sejw.al>
2023-06-29 21:27:14 +0530

    improved unfinished route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const unfinished = require('./routes/unfinished');
 const finished = require('./routes/finished');
 const dbConnect = require('./dbConnect');
 const checkSecrets = require('./checkSecrets');
+const validateID = require('./middleware/validateID');
 
 checkSecrets();
 dbConnect();
@@ -16,8 +17,8 @@ app.use(express.json());
 app.use('/api/todos', todos);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
-app.use('/api/:id/unfinished', unfinished);
-app.use('/api/:id/finished', finished);
+app.use('/api/:id/unfinished', validateID, unfinished);
+app.use('/api/:id/finished', validateID, finished);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('port:', port));
--- routes/unfinished.js
+++ routes/unfinished.js
@@ -1,51 +1,55 @@
 const express = require('express');
-const Todo = require('../models/Todo');
+const { Todo } = require('../models/Todo');
 const router = express.Router();
 
 // get all the unfinished items from a todo
 router.get('/', async (req, res) => {
+  // find todo
   const todoID = req.params.id;
   const todo = await Todo.findById(todoID);
-
+  if (!todo) return res.status(404).send('todo not found');
+  // get all the unfinished items
   const unfinishedItems = todo.unfinishedItems;
   res.status(200).send(unfinishedItems);
 });
 
 // add items to unfinished array
 router.post('/', async (req, res) => {
-  const todoID = req.params.id;
   let items = req.body.items;
   // use space to add multiple items
   items = items.trim().split(' ');
-
-  const todo = await Todo.findById(todoID);
+  // find todo
+  const todo = await Todo.findById(req.params.id);
+  if (!todo) return res.status(404).send('todo not found');
+  // add items to todo
   todo.unfinishedItems.push(...items);
   await todo.save();
-  res.send(todo);
+  res.status(200).send(todo);
 });
 
 // update an item in unfinished
-router.put('/:item/:newVal', async (req, res) => {
-  const todoID = req.params.id;
-  const item = req.params.item;
-  const newVal = req.params.newVal;
-
-  const todo = await Todo.findById(todoID);
+router.put('/', async (req, res) => {
+  // find todo
+  const todo = await Todo.findById(req.params.id);
   if (!todo) return res.status(404).send('todo not found');
-  const indexOfItem = todo.unfinishedItems.indexOf(item);
-  if (indexOfItem === -1) return res.status(500).send('item does not exist');
-  todo.unfinishedItems.splice(indexOfItem, 1, newVal);
+  // find item
+  const indexOfItem = todo.unfinishedItems.indexOf(req.body.item);
+  if (indexOfItem === -1) return res.status(404).send('item does not exist');
+  // replace item with the new item
+  todo.unfinishedItems.splice(indexOfItem, 1, req.body.newItem);
   await todo.save();
   res.status(200).send(todo);
 });
 
 // delete an item from unfinished
 router.delete('/:item', async (req, res) => {
-  const todoID = req.params.id;
-  const item = req.params.item;
-
-  let todo = await Todo.findById(todoID);
-  const indexOfItem = todo.unfinishedItems.indexOf(item);
+  // find todo
+  let todo = await Todo.findById(req.params.id);
+  if (!todo) return res.status(404).send('todo not found');
+  // find item
+  const indexOfItem = todo.unfinishedItems.indexOf(req.params.item);
+  if (indexOfItem === -1) return res.status(404).send('item does not exist');
+  // delete item
   todo.unfinishedItems.splice(indexOfItem, 1);
   todo = await todo.save();
   res.status(200).send(todo);
