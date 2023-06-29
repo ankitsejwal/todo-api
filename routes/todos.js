9593a7072b5fc8ba21e98dafe6744404611c0ff8
ankitsejwal <ankit@sejw.al>
2023-06-29 15:06:45 +0530

    fix todos route

--- routes/todos.js
+++ routes/todos.js
@@ -1,8 +1,6 @@
-const express = require('express');
-const Joi = require('joi');
+const router = require('express').Router();
 const Todo = require('../models/Todo');
-
-const router = express.Router();
+const validate = require('../middleware/validate');
 
 // get all the todos
 router.get('/', async (req, res) => {
@@ -10,67 +8,47 @@ router.get('/', async (req, res) => {
   res.status(200).send(todos);
 });
 
-// get a todo by name
-router.get('/:name', async (req, res) => {
-  let todo = await Todo.findOne({ name: req.params.name });
-  // if name is incorrect
-  if (!todo) return res.status(500).send('incorrect name');
-  // else
-  res.status(200).send(todo);
+// get a single todo
+router.get('/:id', validate, async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    if (!todo) return res.status(404).send('todo not found');
+    res.status(200).send(todo);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 // create a new todo
-router.post('/', async (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).max(15).required(),
-    unfinishedItems: Joi.array().items(Joi.string().min(3).max(15)),
-    finishedItems: Joi.array().items(Joi.string().min(3).max(15)),
-  });
-
-  let todo = {
-    name: req.body.name,
-    unfinishedItems: req.body.unfinishedItems,
-    finishedItems: req.body.finishedItems,
-  };
-
-  const { value, error } = schema.validate(todo);
-  if (error) return res.status(500).send(error.details[0].message);
-  todo = await new Todo(value);
-  todo = await todo.save();
-  res.status(200).send(todo);
-});
-
-// get a single todo
-router.get('/:id', async (req, res) => {
+router.post('/', validate, async (req, res) => {
   try {
-    const todo = await Todo.findById(req.params.id);
+    const todo = await new Todo(req.body);
+    await todo.save();
     res.status(200).send(todo);
-  } catch (err) {
-    console.log(err.message);
-    res.status(404).send('ID not found');
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 
 // update an existing todo
-router.put('/:id', async (req, res) => {
-  const id = req.params.id;
-  let todo = {
-    name: req.body.name,
-    unfinishedItems: req.body.unfinishedItems,
-    finishedItems: req.body.finishedItems,
-  };
-  todo = await Todo.findByIdAndUpdate(id, todo, { new: true });
-  res.status(200).send(todo);
+router.put('/:id', validate, async (req, res) => {
+  try {
+    const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!todo) return res.status(404).send('todo not found');
+    res.status(200).send(todo);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 // delete a todo
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', validate, async (req, res) => {
   try {
     const todo = await Todo.findByIdAndDelete(req.params.id);
+    if (!todo) return res.stats(404).send('todo not found');
     res.status(200).send(todo);
-  } catch (err) {
-    console.log(err.message);
-    res.status(404).send('ID not found');
+  } catch (error) {
+    res.status(400).send(error);
   }
 });
 
