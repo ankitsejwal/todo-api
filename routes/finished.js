ec9a2ae210265db7a175200bd9c1e7112915f518
ankitsejwal <ankit@sejw.al>
2023-06-29 21:35:43 +0530

    fixes on finished route

--- routes/finished.js
+++ routes/finished.js
@@ -1,39 +1,44 @@
-const express = require('express');
-const Todo = require('../models/Todo');
-const router = express.Router();
+const router = require('express').Router();
+const { Todo } = require('../models/Todo');
 
-// get all the finished items from a todo
-router.get('/:todoID', async (req, res) => {
-  const todoID = req.params.todoID;
-  const todo = await Todo.findById(todoID);
-
+// get all the unfinished items from a todo
+router.get('/', async (req, res) => {
+  // find todo
+  const todo = await Todo.findById(req.params.id);
+  if (!todo) return res.status(404).send('todo not found');
+  // get all the unfinished items
   const finishedItems = todo.finishedItems;
   res.status(200).send(finishedItems);
 });
 
 // add items to finished array
 router.post('/:todoID', async (req, res) => {
-  const todoID = req.params.todoID;
   let item = req.body.item;
-
-  let todo = await Todo.findById(todoID);
+  // find todo
+  const todo = await Todo.findById(req.params.id);
+  if (!todo) return res.status(404).send('todo not found');
+  // find item
   const indexOfItem = todo.unfinishedItems.indexOf(item);
   if (indexOfItem === -1) return res.status(404).send('item not found');
+  // move item from unfinished to finished
   todo.unfinishedItems.splice(indexOfItem, 1);
   todo.finishedItems.push(item);
-  todo = await todo.save();
+  await todo.save();
   res.send(todo);
 });
 
 // delete an item from finished
-router.delete('/:todoID/:item', async (req, res) => {
-  const todoID = req.params.todoID;
+router.delete('/:item', async (req, res) => {
   const item = req.params.item;
-
-  let todo = await Todo.findById(todoID);
+  // find todo
+  const todo = await Todo.findById(todoID);
+  if (!todo) return res.status(404).send('todo not found');
+  // find item
   const indexOfItem = todo.finishedItems.indexOf(item);
+  if (indexOfItem === -1) return res.status(404).send('item not found');
+  // delete item
   todo.finishedItems.splice(indexOfItem, 1);
-  todo = await todo.save();
+  await todo.save();
   res.status(200).send(todo);
 });
 
--- routes/unfinished.js
+++ routes/unfinished.js
@@ -1,12 +1,10 @@
-const express = require('express');
+const router = require('express').Router();
 const { Todo } = require('../models/Todo');
-const router = express.Router();
 
 // get all the unfinished items from a todo
 router.get('/', async (req, res) => {
   // find todo
-  const todoID = req.params.id;
-  const todo = await Todo.findById(todoID);
+  const todo = await Todo.findById(req.params.id);
   if (!todo) return res.status(404).send('todo not found');
   // get all the unfinished items
   const unfinishedItems = todo.unfinishedItems;
@@ -56,3 +54,5 @@ router.delete('/:item', async (req, res) => {
 });
 
 module.exports = router;
+
+// transfer unfinshed to finished
